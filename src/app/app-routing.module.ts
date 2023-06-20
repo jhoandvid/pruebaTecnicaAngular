fd09afa25b3c0c8e150053d657b0021c2310d219
jhoan david <rojasalmariojhoandvid@gmail.com>
2023-06-20 17:19:12 -0500

    creation of dashboard components and route review

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthPageComponent } from './modules/auth/auth-page/auth-page.component';
+import { HomePageComponent } from './modules/home/home-page/home-page.component';
 
 const routes: Routes = [
   {
     path: 'auth',
-    component: AuthPageComponent 
+    component: AuthPageComponent ,
+  },
+
+  {
+    path: '',
+    component: HomePageComponent,
+    loadChildren: ()=>import('./modules/home/home.module').then(m=>m.HomeModule),
   }
 ];
 
