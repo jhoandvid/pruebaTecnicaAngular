53e4b6dac71659603398a989aeca22aa1ffa6420
jhoan david <rojasalmariojhoandvid@gmail.com>
2023-06-20 23:10:40 -0500

    it was extracted least Affected and most Affected

--- src/app/modules/home/dashboard/dashboard.component.ts
+++ src/app/modules/home/dashboard/dashboard.component.ts
@@ -6,6 +6,8 @@ import * as Papa from 'papaparse';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  leastAffected:any=[];
+  mostAffected:any=[];
 
   public onFileChange(event: any) {
     let reader = new FileReader();
@@ -16,11 +18,35 @@ export class DashboardComponent {
       reader.onload = () => {
         let csvData = reader.result;
         let parsedData = Papa.parse(csvData as string).data;
-
-        console.log(parsedData);
+        this.mostAndLeastAffectedStates(parsedData);
+          
       };
     }
   }
 
+  mostAndLeastAffectedStates(data:any) {
+    const cities:any={};
+    let result;
+
+    data.forEach((city:any)=>{
+      if (!isNaN(city[city.length - 1])) {
+        if (!cities[city[6]]) {
+        cities[city[6]] = {
+          state: city[6], 
+          countDate: Number(city[city.length - 1])  
+        }
+      } else {
+        cities[city[6]].state; 
+        cities[city[6]].countDate +=Number(city[city.length - 1])
+      } 
+    }
+    });
+
+    const sortedCities=Object.values(cities).map((res:any)=>({state:res.state, total:res.countDate})).sort((a, b)=>a.total-b.total);
+    this. leastAffected = sortedCities.filter((city: any) => city.total === sortedCities[0].total);
+    this. mostAffected = sortedCities.filter((city: any) => city.total === sortedCities[sortedCities.length - 1].total);
+
+
+}
 
 }
